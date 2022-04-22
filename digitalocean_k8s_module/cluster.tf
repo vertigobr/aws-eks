b00d1a6cc37835d517b4661e4945012471352ab4
João Pedro Pedroza <jfurtado@vertigo.com.br>
2022-04-22 11:37:30 -0300

    Add variables into CI pipeline and fix k8s version

--- digitalocean_k8s_module/cluster.tf
+++ digitalocean_k8s_module/cluster.tf
@@ -11,18 +11,4 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
     min_nodes  = var.node_pool_default.min_nodes
     max_nodes  = var.node_pool_default.max_nodes
   }
-
-}
-
-resource "digitalocean_kubernetes_node_pool" "additional_node_pools" {
-  for_each = var.additional_node_pools
-
-  cluster_id = digitalocean_kubernetes_cluster.cluster.id
-  name       = each.value.name
-  size       = each.value.size
-  node_count = each.value.node_count
-  auto_scale = each.value.auto_scale
-  min_nodes  = each.value.min_nodes
-  max_nodes  = each.value.max_nodes
-
 }
--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "digitalocean" {
 module "digitalocean_k8s_cluster" {
   source = "./digitalocean_k8s_module"
 
-  cluster_name          = local.config.cluster_name
-  region                = local.config.region
-  prefix_version        = local.config.prefix_version
+  cluster_name          = var.cluster_name == null ? local.config.cluster_name : var.cluster_name
+  prefix_version        = var.prefix_version == null ? local.config.prefix_version : var.prefix_version
+  region                = var.region == null ? local.config.region : var.region
   node_pool_default     = local.config.node_pool_default
   additional_node_pools = local.config.additional_node_pools
 }
