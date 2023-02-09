12428b365698c670731584a9e7f2325fbdce842a
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-02-09 19:22:37 -0300

    Ajuste Final Driver EBS Cluster EKS

--- main.tf
+++ main.tf
@@ -38,37 +38,85 @@ module "kubeconfig" {
   depends_on = [module.kubernetes]
 }
     
- data "aws_eks_cluster" "eks" {
-  name              = local.config.cluster_name
-  depends_on = [module.cluster]
-}
+# data "aws_eks_cluster" "eks" {
+#  name              = local.config.cluster_name
+#  depends_on = [module.cluster]
+#}
 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-  depends_on = [module.cluster]
-}
+#module "oidc-provider-data_example_eks-oidc-role" {
+#  source  = "reegnz/oidc-provider-data/aws//examples/eks-oidc-role"
+#  version = "0.0.4"
+#  eks_cluster_name = local.config.cluster_name
+#  eks_sa_name      = local.config.cluster_name
+#  eks_sa_namespace = "kube-system"
+#  role_name        = "AmazonEBSCSIDriverPolicy"
+#  depends_on = [module.cluster]
+#}
+#data "aws_iam_policy" "ebs_csi_policy" {
+#  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+#  depends_on = [module.cluster]
+#}
  
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
+# module "irsa-ebs-csi" {
+#  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+#  version = "4.7.0"
+#
+#  create_role                   = true
+#  role_name                     = "AmazonEKSTFEBSCSIRole-${local.config.cluster_name}"
+#  provider_url                  = replace(data.aws_eks_cluster.eks.identity.0.oidc.0.issuer, "https://", "")
+#  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+#  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+#  depends_on = [module.kubernetes]
+#}
 
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${local.config.cluster_name}"
-  provider_url                  = replace(data.aws_eks_cluster.eks.identity.0.oidc.0.issuer, "https://", "")
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-  depends_on = [module.kubernetes]
+#resource "aws_eks_addon" "ebs-csi" {
+#  cluster_name             = local.config.cluster_name
+#  addon_name               = "aws-ebs-csi-driver"
+#  addon_version            = "v1.14.0-eksbuild.1"
+#  preserve                 = true
+#  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+#  tags = {
+#    "eks_addon" = "ebs-csi"
+#    "terraform" = "true"
+#  }
+#  depends_on = [module.cluster]  
+#}
+
+data "aws_eks_cluster" "cluster" {
+  name = local.config.cluster_name
+  depends_on = [module.cluster]  
 }
 
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = local.config.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.5.2-eksbuild.1"
-  preserve                 = true
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.config.cluster_name
+  depends_on = [module.cluster]  
+}
+
+data "tls_certificate" "cert" {
+  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+  depends_on = [module.cluster]  
+}
+
+resource "aws_iam_openid_connect_provider" "openid_connect" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.cert.certificates.0.sha1_fingerprint]
+  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+  depends_on = [module.cluster]  
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+module "ebs_csi_driver_controller" {
+  source = "DrFaust92/ebs-csi-driver/kubernetes"
+  version = "3.5.0"
+
+  ebs_csi_controller_image                   = ""
+  ebs_csi_controller_role_name               = "ebs-csi-driver-controller"
+  ebs_csi_controller_role_policy_name_prefix = "ebs-csi-driver-policy"
+  oidc_url                                   = aws_iam_openid_connect_provider.openid_connect.url
   depends_on = [module.cluster]  
 }
