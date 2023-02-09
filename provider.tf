3788b28233f5f82ef096daec827dda47ebb136f6
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-02-09 19:23:04 -0300

    Ajuste provider

--- provider.tf
+++ provider.tf
@@ -4,8 +4,8 @@ provider "aws" {
   region     = var.aws_region
 }
 
-provider "kubernetes" {
-  host                   = module.cluster.cluster_endpoint
-  cluster_ca_certificate = module.cluster.cluster_certificate_authority_data
-  token                  = module.cluster.kubeconfig_token
-}
+#provider "kubernetes" {
+#  host                   = module.cluster.cluster_endpoint
+#  cluster_ca_certificate = module.cluster.cluster_certificate_authority_data
+#  token                  = module.cluster.kubeconfig_token
+#}
