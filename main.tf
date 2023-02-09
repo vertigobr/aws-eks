e3d3dd7058fd795d355499d5ce561572ca4b0f63
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-02-09 19:24:23 -0300

    Ajuste EBS CSI DRIVER EKS

--- main.tf
+++ main.tf
@@ -38,50 +38,6 @@ module "kubeconfig" {
   depends_on = [module.kubernetes]
 }
     
-# data "aws_eks_cluster" "eks" {
-#  name              = local.config.cluster_name
-#  depends_on = [module.cluster]
-#}
-
-#module "oidc-provider-data_example_eks-oidc-role" {
-#  source  = "reegnz/oidc-provider-data/aws//examples/eks-oidc-role"
-#  version = "0.0.4"
-#  eks_cluster_name = local.config.cluster_name
-#  eks_sa_name      = local.config.cluster_name
-#  eks_sa_namespace = "kube-system"
-#  role_name        = "AmazonEBSCSIDriverPolicy"
-#  depends_on = [module.cluster]
-#}
-#data "aws_iam_policy" "ebs_csi_policy" {
-#  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-#  depends_on = [module.cluster]
-#}
- 
-# module "irsa-ebs-csi" {
-#  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-#  version = "4.7.0"
-#
-#  create_role                   = true
-#  role_name                     = "AmazonEKSTFEBSCSIRole-${local.config.cluster_name}"
-#  provider_url                  = replace(data.aws_eks_cluster.eks.identity.0.oidc.0.issuer, "https://", "")
-#  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-#  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-#  depends_on = [module.kubernetes]
-#}
-
-#resource "aws_eks_addon" "ebs-csi" {
-#  cluster_name             = local.config.cluster_name
-#  addon_name               = "aws-ebs-csi-driver"
-#  addon_version            = "v1.14.0-eksbuild.1"
-#  preserve                 = true
-#  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-#  tags = {
-#    "eks_addon" = "ebs-csi"
-#    "terraform" = "true"
-#  }
-#  depends_on = [module.cluster]  
-#}
-
 data "aws_eks_cluster" "cluster" {
   name = local.config.cluster_name
   depends_on = [module.cluster]  
